dce95db51f23066b7e006acfec48708c8b29bd08
Abdelrahman <Aa5282243@gmail.com>
2025-09-16 22:49:43 +0300

    - Finish Home Screen bottom navigation bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: _currentIndex == 0 ? Container() : Placeholder(),
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (value) => setState(() {
+        _currentIndex = value;
+      }),
+
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add, size: 34, color: Colors.black),
+          label: 'Add Order',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dashboard, size: 34, color: Colors.black),
+          label: 'Dashboard',
+        ),
+      ],
+    );
+  }
+}
